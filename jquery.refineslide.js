42773441f3cea373d98464c51e96b2eecd57ec14
Arindam Chakraborty <arismart99@gmail.com>
2013-02-07 23:11:35 +0530

    Making custom animations cycle through next and prev actions

--- jquery.refineslide.js
+++ jquery.refineslide.js
@@ -669,11 +669,10 @@
         }
 
         ,custom: function() {
-            var animIndex = this.RS.nextAnimIndex;
-            if(animIndex < 0) animIndex = 0;
-            if(animIndex > 0) animIndex = animIndex % this.customAnims.length;
+            if(this.RS.nextAnimIndex < 0) this.RS.nextAnimIndex = this.customAnims.length - 1;
+            if(this.RS.nextAnimIndex === this.customAnims.length) this.RS.nextAnimIndex = 0;
             // Pick the next item in the list of transitions provided by user.
-            this[this.customAnims[animIndex]]();
+            this[this.customAnims[this.RS.nextAnimIndex]]();
         }
     };
 
